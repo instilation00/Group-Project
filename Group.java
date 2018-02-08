b83d852bbc2500453b03b0a29e333b1c91fa3bff
Eneficial <wetzelhm@students.osd.wednet.edu>
2018-02-08 12:30:58 -0800

    Update Group.java

--- Group.java
+++ Group.java
@@ -11,137 +11,75 @@
 //
 //Creativity Points:
 //Witty responses for the letter grades
-//Grade input instead of having the grades hard-coded in grades.txt - done via inputGrade()
-// http://www.java67.com/2016/07/how-to-read-text-file-into-arraylist-in-java.html
-import java.io.*;
+//Grade input instead of having the grades hard-coded in grades.txt - done in main method
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 public class LetterGrade {
-	public static void main(String[] args) //TODO: Remember to give credit for this since we're pretty much copying this code from the text
-    {
-       int numTests;     // The number of tests
-       int[] tests;      // Array of test scores
-
-       // Create a Scanner object for keyboard input.
-       Scanner keyboard = new Scanner(System.in);
-
-       // Get the number of test scores.
-       System.out.print("How many tests do you have? ");
-       numTests = keyboard.nextInt();
-
-       // Create an array to hold that number of scores.
-       tests = new int[numTests];
-       
-       // Get the individual test scores.
-              for (int index = 0; index < tests.length; index++)
-       {
-          System.out.print("Enter test score " +
-                           (index + 1) + ": ");
-          tests[index] = keyboard.nextInt();
-       }
-
-       // Display the test scores.
-       System.out.println();
-       System.out.println("Here are the scores you entered:");
-       for (int index = 0; index < tests.length; index++)
-          System.out.print(tests[index] + " ");
-    }		
 	
-	/*public static void main(String[] args) throws IOException  { //Read from file, then display it.
-		inputGrade();
-		 double test1 = 0;
-		 double test2 = 0;
-		 double test3 = 0;
-		 double test4 = 0;
-		 double test5 = 0;
-		 
-		 String displayOutput = "";
-		 
-		 File file = new File("grades.txt"); 
-		 PrintWriter outputFile = new PrintWriter(file); 
-		 Scanner inputFile = new Scanner(file); 
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		PrintWriter outputFile = new PrintWriter("grades.txt");
+		int numberOfScores = 5;
+		double testInput;
+		double test1 = 0;
+		double test2 = 0;
+		double test3 = 0;
+		double test4 = 0;
+		double test5 = 0;
+		String displayString = "Score & Letter Grade";
 		
-		//inputGrade();
-		while (inputFile.hasNext())
-		{
-			test1 = inputFile.nextDouble();
-			displayOutput += test1 + determineGrade(test1); //Why is this not displaying as it should?
+		for(int totalTests = 1; totalTests <= numberOfScores; totalTests++) {
+			System.out.println("Please enter score " + totalTests);
+			testInput = scanner.nextDouble();
 			
-			test2 = inputFile.nextDouble();
-			displayOutput += test2 + determineGrade(test2);
-			
-			test3 = inputFile.nextDouble();
-			displayOutput += test3 + determineGrade(test3);
-
-			test4 = inputFile.nextDouble();
-			displayOutput += test4 + determineGrade(test4);
-
-			test5 = inputFile.nextDouble();
-			displayOutput += test5 + determineGrade(test5);
+			if(totalTests == 1) {
+				test1 = testInput;
+				outputFile.println(test1);
+				displayString += test1 + determineGrade(test1);
+			} else if (totalTests == 2) {
+			    test2 = testInput;
+				outputFile.println(test2);
+				displayString += test2 + determineGrade(test2);
+			} else if (totalTests == 3) {
+				test3 = testInput;
+				 outputFile.println(test3);
+				 displayString += test3 + determineGrade(test3);
+			} else if (totalTests == 4) {
+				test4 = testInput;
+				 outputFile.println(test4);
+				displayString += test4 + determineGrade(test4);
+			} else if (totalTests == 5) {
+				test5 = testInput;
+				 outputFile.println(test5);
+				displayString += test5 + determineGrade(test5);
+			}
 
 		}
-		System.out.print(displayOutput + "Averages: " + calcAverage(test1, test2, test3, test4, test5)); //Why is this part not being displayed?
-		 inputFile.close(); 
-		 
+		System.out.print(displayString + "Average: " + calcAverage(test1, test2, test3, test4, test5));
 	}
 	
-	
-	public static void inputGrade() throws IOException {
-		
-		
-		Scanner Keyboard = new Scanner(System.in);
-	    double test1;
-	    double test2;
-	    double test3;
-	    double test4;
-	    double test5;
-	    
-	    System.out.print("What is your first test score?");
-	    test1 = Keyboard.nextDouble();
-	    
-	    System.out.print("What is your second test score?");
-	    test2 = Keyboard.nextDouble();
-	    
-	    System.out.print("What is your third test score?");
-	    test3 = Keyboard.nextDouble();
-	    
-	    System.out.print("What is your fourth score?");
-	    test4 = Keyboard.nextDouble();
-	    
-	    System.out.print("What is your fifth test score?");
-	    test5 = Keyboard.nextDouble();
-	    
-	    PrintWriter outputFile = new PrintWriter("grades.txt");
-	    outputFile.println(test1);
-	    outputFile.println(test2);
-	    outputFile.println(test3);
-	    outputFile.println(test4);
-	    outputFile.println(test5);
-	    outputFile.close();
-	}
-	
-	public static double calcAverage(double test1, double test2, double test3, double test4, double test5) {
-		
-	    double Average;
-	    Average = ((test1 + test2 + test3 + test4 + test5)/5);
-	    return Average; //Why is the average always 0? Should be pulling correctly...
-	}
-	
-	public static String determineGrade(double testScores) {
+	public static String determineGrade(double testScore) {
 		String convertedGrade = "";
-		if (testScores >= 90) {
+		if (testScore >= 90) {
 			convertedGrade = "A";
-		} else if (testScores > 80 && testScores < 89) {
+		} else if (testScore > 80 && testScore < 89) {
 			convertedGrade = "B";
-		} else if (testScores > 70 && testScores < 79) {
+		} else if (testScore > 70 && testScore < 79) {
 			convertedGrade = "C";
-		} else if (testScores > 60 && testScores < 69) {
+		} else if (testScore > 60 && testScore < 69) {
 			convertedGrade = "D";
-		} else if (testScores < 60) {
+		} else if (testScore < 60) {
 			convertedGrade = "F";
 		}
 		return convertedGrade;
-	}*/
+	}
 	
-   
-	 
+	
+	public static double calcAverage(double test1, double test2, double test3, double test4, double test5) {
+		double average;
+		average = ((test1 + test2 + test3 + test4 + test5)/5);
+		return average;
+	}
+
 }
