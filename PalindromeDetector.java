3b991c5d2d0362281c6fe8a944b5b888aef89b27
instilation00 <36173350+instilation00@users.noreply.github.com>
2018-05-16 13:56:03 -0700

    Update PalindromeDetector.java

--- PalindromeDetector.java
+++ PalindromeDetector.java
@@ -1,19 +1,33 @@
 //Java Program by Holly Wetzel & Blake Bradley
 import java.util.Stack;
+import java.io.* ;
+import java.util.Scanner; 
+import javax.swing.JOptionPane;
 public class PalindromeDetector {
 
 	public static void main(String[] args) {
 		
-		//Hard coded palindromes to use as test cases
-		String p1 = "racecar";
-		String p2 = "kayak";
-		String p3 = "able was i ere i saw elba";
-		String p4 = "not a palindrome";
+		try
+		{
+		 FileInputStream textFile = new FileInputStream ("data.txt");
+		 Scanner inFile = new Scanner (textFile);
+		 System.out.println("File data.txt has been opened.");
+		}
+		catch (FileNotFoundException fnfe)
+		{
+		 System.out.println("File data.txt was not found!");
+		}
 		
-		System.out.println("Test case 1 is " + palindromeTester(p1));
-		System.out.println("Test case 2 is " + palindromeTester(p2));
-		System.out.println("Test case 3 is " + palindromeTester(p3));
-		System.out.println("Test case 4 is " + palindromeTester(p4));
+		//Hard coded palindromes to use as test cases
+		String p1 = JOptionPane.showInputDialog("racecar");
+		String p2 = JOptionPane.showInputDialog("kayak");
+		String p3 = JOptionPane.showInputDialog("able was i ere i saw elba");
+		String p4 = JOptionPane.showInputDialog("not a palindrome");
+		
+		System.out.println("Test case 1 is " + palindromeTester(p1) + " for " + p1);
+		System.out.println("Test case 2 is " + palindromeTester(p2) + " for " + p2);
+		System.out.println("Test case 3 is " + palindromeTester(p3) + " for " + p3);
+		System.out.println("Test case 4 is " + palindromeTester(p4) + " for " + p4);
 
 	}
 	
@@ -33,5 +47,4 @@ public class PalindromeDetector {
 		}
 		return s1.equals(s2);
 	}
-
 }
